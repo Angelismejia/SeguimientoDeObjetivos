835ee5392d44dc0899c5e0b6b9acff514470421d
Angelismejia <Angelismejia06@gmail.com>
2026-07-02 15:52:27 -0400

    feat: implement navbar component with authentication logic and logout functionality

--- Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  name: string = '';
+
+  constructor(private authService: AuthService) {
+    this.name = this.authService.getName();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
